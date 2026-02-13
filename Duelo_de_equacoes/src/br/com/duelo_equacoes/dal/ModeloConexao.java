0141675abbaaccdd580a0e75d80e3fffceb62cac
Julimar Soares <julimarmeira21@gmail.com>
2026-02-12 22:40:52 -0300

    Criação da classe conexão
    Criação da classe responsável por conectar o projeto com a base de dados

---
+++ Duelo_de_equacoes/src/br/com/duelo_equacoes/dal/ModeloConexao.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.duelo_equacoes.dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Julimar
+ */
+public class ModeloConexao {
+
+    public static Connection conectar() {
+
+        Connection conexao = null;
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/dbjogador";
+        String user = "root";
+        String password = "21041997";
+
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e);
+            return null;
+        }
+
+    }
+
+}
